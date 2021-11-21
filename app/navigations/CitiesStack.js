4e6ae3d508c2c6b9bc1c971e9430b9ce488d9785
Fernando Emanuel Díaz Machado <fernandodiazmachado@gmail.com>
2021-11-21 13:13:33 -0300

    add button plus in screens cities

--- app/navigations/CitiesStack.js
+++ app/navigations/CitiesStack.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
-import Cities from "../screens/Cities";
+import Cities from "../screens/Cities/Cities";
 
 const Stack = createStackNavigator();
 
