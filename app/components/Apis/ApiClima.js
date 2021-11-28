aba0a4cb7bbd5653c1774de331612195cd989c1f
irinachurko <87023017+irinachurko@users.noreply.github.com>
2021-11-28 14:10:35 -0300

    Se diseña pagina

--- app/components/Apis/ApiClima.js
+++ app/components/Apis/ApiClima.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import { Text, View, Image} from "react-native";
+import { Text, View, Image, StyleSheet, ImageBackground} from "react-native";
 
 
 
@@ -36,16 +36,45 @@ const ApiClima = (props)=>{
     
     return(
         <View>
-            <Text>{descripcion}</Text>
-            <Text>Temperatura Actual: {clima.temp}</Text>
-            <Text>Sensación Térmica: {clima.feels_like}</Text>
-            <Text>Humedad: {clima.humidity}%</Text>
+           
+            
+            
+            <Text style={styles.description} >Temperatura Actual: {clima.temp}°</Text>
+            <Text style={styles.description}>Sensación Térmica: {clima.feels_like}°</Text>
+            <Text style={styles.description}>Humedad: {clima.humidity}%</Text>
+            <Text style={styles.subDescription}>{descripcion}</Text>
             <Image
                 source={{ uri: urlImage }}
-                style={{ width: 100, height: 100 }}/>
+                style={{ width: 150, height: 100, alignSelf:"center", marginTop:15}}/>
+        
         </View>
        
     );
 }
+
+const styles = StyleSheet.create({
+    
+    subDescription: {
+        color: "#f2f2f2",
+        textTransform:"uppercase",
+        fontSize: 19,
+        fontWeight: "700",
+        marginLeft: 15,
+        marginRight: 15,
+        marginBottom: 20,
+        textAlign: "center"
+    
+    },
+
+    description: {
+        color: "#f2f2f2",
+        fontSize: 19,
+        fontWeight: "500",
+        marginLeft: 15,
+        marginRight: 15,
+        marginBottom: 20,
+        textAlign: "center"
+    }
+});
 
 export default ApiClima;
